f4807eb1c88b6a2258dee337317b0e51032669f4
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-30 03:14:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,6 +11,10 @@ def happy_birthday(birthday_kids)
  end
 end
 
-def age_appropriate_birthday(name, age)
-
+def age_appropriate_birthday(birthday_kids)
+  if age < 12
+    birthday_kids.each do |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
 end
